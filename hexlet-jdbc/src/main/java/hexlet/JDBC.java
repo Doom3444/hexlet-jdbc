1690d18a9999a2c6894a566eff1a52366fbd493f
Doom3444 <kailomailo3444@gmail.com>
2024-04-08 21:08:51 +0300

    add User.java and UserDAO.java

--- hexlet-jdbc/src/main/java/hexlet/JDBC.java
+++ hexlet-jdbc/src/main/java/hexlet/JDBC.java
@@ -2,37 +2,25 @@ package hexlet;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class JDBC {
 
     public static void main(String[] args) throws SQLException {
 
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        var connection = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
 
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+        var dao = new UserDAO(connection);
 
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+        var user1 = new User("Tom", "1111");
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy_1', '123456789')";
-        var sql3 = "INSERT INTO users (username, phone) VALUES ('tommy_2', '123456789')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.executeUpdate(sql3);
-        statement2.close();
+        System.out.println(user1.getId());
+        dao.save(user1);
+        System.out.println(user1.getId());
 
-        var sql4 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
+        var user2 = dao.find(user1.getId()).get();
+        System.out.println(user1.getId() == user2.getId());
 
-        var resultSet = statement3.executeQuery(sql4);
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
-        }
-        statement3.close();
-
-        conn.close();
+        connection.close();
     }
 }
---
+++ hexlet-jdbc/src/main/java/hexlet/User.java
@@ -0,0 +1,37 @@
+package hexlet;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String phone;
+
+    public User(String username, String phone) {
+        this.username = username;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public void setName(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
